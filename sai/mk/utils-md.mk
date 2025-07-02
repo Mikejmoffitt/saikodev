9ee0ccf5fe6b835dcbdfba54f05446e780d1bf33
Michael Moffitt <mikejmoffitt@gmail.com>
2025-07-02 16:53:58 +0900

    megaloader def

--- sai/mk/utils-md.mk
+++ sai/mk/utils-md.mk
@@ -1,6 +1,8 @@
 
+MEGALOADER := $(UTILDIR)/debug/megaloader
+
 $(MEGALOADER): $(UTILDIR)/debug/megaloader.c
-	@$(CC_HOST) -D_DEFAULT_SOURCE $< -o $@ $(HOSTCFLAGS)
+	$(CC_HOST) -D_DEFAULT_SOURCE $< -o $@ $(HOSTCFLAGS)
 
 flash: $(OUTDIR)/$(PROJECT_NAME).bin $(MEGALOADER)
 	$(MEGALOADER) md $< /dev/ttyUSB0 2> /dev/null
